3d60398307a12955fe4f0dde4ddab232a7213f3f
Michael Delcore <nmdelcore@gmail.com>
2021-09-16 10:17:02 +0200

    instantiating superhero entry and classes - also made edits to hmtl and css files

--- app.js
+++ app.js
@@ -9,18 +9,28 @@ class SuperheroEntry {
 }
 
 // The Superhero List Class
-class SuperheroList {}
+class SuperheroList {
+  addSuperhero(entry);
+}
 
 
 // ---------------------- Events ---------------------------
 
 const form = document.querySelector('.superhero-form');
 form.addEventListener('submit', function(e) {
-  e.preventDefault()
+  e.preventDefault();
 
   let [superheroName, superheroUniverse, superheroPower] = [
     document.querySelector('#name').value,
     document.querySelector('#universe').value,
     document.querySelector('#power').value,
-  ]
-})
+  ];
+
+  // Instantiating the superheroEntry Class
+  const entry = new SuperheroEntry(superheroName, superheroUniverse, superheroPower);
+
+  // Instantiating the superheroList Class
+  const list = new SuperheroList();
+
+  list.addSuperhero(entry);
+});
