f6d94375667bc806ba41fdd5ef973984e67684b3
Michael Delcore <nmdelcore@gmail.com>
2021-09-16 09:50:49 +0200

    added superhero entry and list classes

---
+++ app.js
@@ -0,0 +1,14 @@
+// The Superhero Entry
+class SuperheroEntry {
+  constructor(superheroName, superheroUniverse, superheroPower) {
+    this.superheroName = superheroName;
+    this.superheroUniverse = superheroUniverse;
+    this.superheroPower = superheroPower;
+
+  }
+}
+
+// The Superhero List Class
+class SuperheroList {
+  
+}
