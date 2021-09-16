eb5f447ba9409cffe79b4630b47aab672f7dc1b4
Michael Delcore <nmdelcore@gmail.com>
2021-09-16 09:58:06 +0200

    created the form submission event listener

--- app.js
+++ app.js
@@ -9,6 +9,18 @@ class SuperheroEntry {
 }
 
 // The Superhero List Class
-class SuperheroList {
-  
-}
+class SuperheroList {}
+
+
+// ---------------------- Events ---------------------------
+
+const form = document.querySelector('.superhero-form');
+form.addEventListener('submit', function(e) {
+  e.preventDefault()
+
+  let [superheroName, superheroUniverse, superheroPower] = [
+    document.querySelector('#name').value,
+    document.querySelector('#universe').value,
+    document.querySelector('#power').value,
+  ]
+})
